d5cebebfc98157a479b9fe7a74aa037706bf4b11
Amilcar Sanchez <amilcaralex97@gmail.com>
2024-02-07 18:35:05 -0600

    feat(infrastructure): api stack reference removed

--- bin/evo_motors_api.ts
+++ bin/evo_motors_api.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { EvoMotorsApiStack } from "../lib/Stacks/evo_motors_api-stack";
 import { EvoMotorsCiCdStack } from "../lib/Stacks/evo_motors_cicd-stack";
 
 const app = new cdk.App();
-new EvoMotorsApiStack(app, "EvoMotorsApiStack", {});
 new EvoMotorsCiCdStack(app, "EvoMotorsCiCdStack", {});
 
 app.synth();
