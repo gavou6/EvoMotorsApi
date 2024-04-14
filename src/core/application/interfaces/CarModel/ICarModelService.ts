d32c8c141dbf91d20166e8e1cef137f82e55ed8c
Amilcar Sanchez <amilcaralex97@gmail.com>
2024-04-14 13:15:18 -0600

    feat(CarModel): Service first implementation

--- src/core/application/interfaces/CarModel/ICarModelService.ts
+++ src/core/application/interfaces/CarModel/ICarModelService.ts
@@ -1,10 +1,10 @@
 import { CarModel } from "../../../domain/entities/CarModel";
 import { CreateCarModelDTO, UpdateCarModelDTO } from "../../dtos/CarModel";
 
-export interface IBrandService {
+export interface ICarModelService {
   createCarModel(dto: CreateCarModelDTO): Promise<CarModel>;
   getCarModelById(id: string): Promise<CarModel | null>;
   getAllCarModels(): Promise<CarModel[]>;
-  updateCarModel(dto: UpdateCarModelDTO): Promise<CarModel | null>;
+  updateCarModel(id: string, dto: UpdateCarModelDTO): Promise<CarModel | null>;
   deleteCarModel(id: string): Promise<void>;
 }
---
+++ src/core/domain/services/CarModelService.ts
@@ -0,0 +1,36 @@
+import { Brand } from "../entities/Brand";
+import { IBrandRepository } from "../../application/interfaces/Brand/IBrandRepository";
+import { ICarModelService } from "../../application/interfaces/CarModel/ICarModelService";
+import { CarModel } from "../entities/CarModel";
+import { ICarModelRepository } from "../../application/interfaces/CarModel/ICarModelRepository";
+import {
+  CreateCarModelDTO,
+  UpdateCarModelDTO,
+} from "../../application/dtos/CarModel";
+
+export class CarModelService implements ICarModelService {
+  constructor(private carModelRepository: ICarModelRepository) {}
+
+  async getCarModelById(id: string): Promise<CarModel | null> {
+    return this.carModelRepository.findById(id);
+  }
+
+  async getAllCarModels(): Promise<CarModel[]> {
+    return this.carModelRepository.findAll();
+  }
+
+  async createCarModel(dto: CreateCarModelDTO): Promise<CarModel> {
+    return this.carModelRepository.save(dto);
+  }
+
+  async updateCarModel(
+    id: string,
+    dto: UpdateCarModelDTO,
+  ): Promise<CarModel | null> {
+    return this.carModelRepository.update(id, dto);
+  }
+
+  async deleteCarModel(id: string): Promise<void> {
+    await this.carModelRepository.deleteById(id);
+  }
+}
