b2cab6bfd0aff4079a1e21bd45333b281585a114
Amilcar Sanchez <amilcaralex97@gmail.com>
2024-04-24 00:09:02 -0600

    feat(Brand): refactor to implement real id in doc transform

--- src/core/domain/entities/Brand.ts
+++ src/core/domain/entities/Brand.ts
@@ -1,15 +1,14 @@
 export class Brand {
-  public id?: string; // Make `id` optional
-
   constructor(
     public name: string,
     public description?: string,
+    public _id?: string,
   ) {}
 
   // Method to set the id if it's not passed in the constructor
   setId(id: string) {
-    if (!this.id) {
-      this.id = id;
+    if (!this._id) {
+      this._id = id;
     }
   }
 }
